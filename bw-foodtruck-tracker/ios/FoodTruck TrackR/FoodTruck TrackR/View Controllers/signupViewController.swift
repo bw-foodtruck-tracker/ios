d1c8233361889b571b16dbca3247e2515a2903f7
Nicolas Rios <nickrios33@yahoo.com>
2020-03-01 18:05:15 -0800

    added launch image

--- bw-foodtruck-tracker/ios/FoodTruck TrackR/FoodTruck TrackR/View Controllers/signupViewController.swift
+++ bw-foodtruck-tracker/ios/FoodTruck TrackR/FoodTruck TrackR/View Controllers/signupViewController.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 
+
 class signupViewController: UIViewController {
+   
     
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var signupButton: UIButton!
     @IBOutlet weak var emailTextField: UITextField!
     
-    // come back to file for
+    // come back to file for more coding
+    
     
     
     
