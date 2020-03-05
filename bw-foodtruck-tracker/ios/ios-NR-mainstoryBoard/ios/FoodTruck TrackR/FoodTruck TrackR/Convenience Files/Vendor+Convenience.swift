586510b213c8588badd2aeb280e74b17676ad56b
Pravin <patelpra337@gmail.com>
2020-03-05 06:00:23 -0600

    Add files

--- bw-foodtruck-tracker/ios/ios-NR-mainstoryBoard/ios/FoodTruck TrackR/FoodTruck TrackR/Convenience Files/Vendor+Convenience.swift
+++ bw-foodtruck-tracker/ios/ios-NR-mainstoryBoard/ios/FoodTruck TrackR/FoodTruck TrackR/Convenience Files/Vendor+Convenience.swift
@@ -9,23 +9,23 @@
 import Foundation
 import CoreData
 
-extension FoodVendor {
-    @discardableResult convenience init(email: String, username: String, password: String, vendorTrucks: [Truck], identifier: UUID = UUID(), context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
-        self.init(context: context)
-        
-        self.email = email
-        self.username = username
-        self.password = password
-        self.vendorTrucks = NSOrderedSet(object: vendorTrucks)
-        self.identifier = identifier
-    }
-    
-    convenience init(user: VendorRepresentation) {
-        var trucks: [Truck] = []
-        for truck in user.vendorTrucks {
-            trucks.append(Truck(truck: truck))
-        }
-        
-        self.init(username: user.username, password: user.password, email: user.email, trucksOwned: trucks)
-    }
-}
+//extension FoodVendor {
+//    @discardableResult convenience init(email: String, username: String, password: String, vendorTrucks: [Truck], identifier: UUID = UUID(), context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+//        self.init(context: context)
+//        
+//        self.email = email
+//        self.username = username
+//        self.password = password
+//        self.vendorTrucks = NSOrderedSet(object: vendorTrucks)
+//        self.identifier = identifier
+//    }
+//    
+//    convenience init(user: VendorRepresentation) {
+//        var trucks: [Truck] = []
+//        for truck in user.vendorTrucks {
+//            trucks.append(Truck(truck: truck))
+//        }
+//        
+//        self.init(username: user.username, password: user.password, email: user.email, trucksOwned: trucks)
+//    }
+//}
