9e934ca7e55da14cdb105883c0aab10b9a0e0871
Nicolas Rios <nickrios33@yahoo.com>
2020-03-04 18:54:40 -0800

    added . swift swift files

---
+++ bw-foodtruck-tracker/ios/ios-NR-mainstoryBoard/ios/FoodTruck TrackR/View Controllers/AddTruckViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddTruckViewController.swift
+//  FoodTruck TrackR
+//
+//  Created by Nicolas Rios on 3/4/20.
+//  Copyright © 2020 Crus Technologies. All rights reserved.
+//
+
+import UIKit
+
+class AddTruckViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
